03ef8084a82145c72c9c8a8a5420f1f3ca17b499
Maki <mxmcube@gmail.com>
2019-02-20 18:13:33 -0800

    Added floor

---
+++ shaders.js
@@ -0,0 +1,16 @@
+var shaders = [
+	{
+		shaderUrl: "https://shaders.cutelab.space/shadertoy/terrainTubes.fs",
+		channels: [
+			"https://www.shadertoy.com/media/a/e6e5631ce1237ae4c05b3563eda686400a401df4548d0f9fad40ecac1659c46c.jpg",
+			"https://www.shadertoy.com/media/a/f735bee5b64ef98879dc618b016ecf7939a5756040c2cde21ccb15e69a6e1cfb.png",
+			"",
+			"https://www.shadertoy.com/media/a/bd6464771e47eed832c5eb2cd85cdc0bfc697786b903bfd30f890f9d4fc36657.jpg"
+		],
+		version: 4
+	},
+	{
+		shaderUrl: "https://shaders.cutelab.space/shadertoy/appollonian.fs",
+		version: 4
+	}
+]
